0f206db4cd4abc5e918582214394238e75bed0c5
RaviDesai <ravidesai@me.com>
2014-12-01 19:15:00 -0500

    initial checkin

---
+++ ITunesRESTFramework/ITunesRESTFramework/ITunesRESTFramework.h
@@ -0,0 +1,19 @@
+//
+//  ITunesRESTFramework.h
+//  ITunesRESTFramework
+//
+//  Created by Ravi Desai on 12/1/14.
+//  Copyright (c) 2014 RSD. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ITunesRESTFramework.
+FOUNDATION_EXPORT double ITunesRESTFrameworkVersionNumber;
+
+//! Project version string for ITunesRESTFramework.
+FOUNDATION_EXPORT const unsigned char ITunesRESTFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ITunesRESTFramework/PublicHeader.h>
+
+
